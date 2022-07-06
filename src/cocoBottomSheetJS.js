f8df79d8abdf786b88169007c59b9b08809de36d
coco <squarehacker@kakao.com>
2022-07-06 13:10:14 +0900

    2022-07-06 13:10:14 업데이트!!!

--- src/cocoBottomSheetJS.js
+++ src/cocoBottomSheetJS.js
@@ -1,9 +1,11 @@
 'use strict';
 class cocoButtomSheetJS{
 	gethtmlcode(code){
+		console.log('gethtmlcode');
 		this.sethtml = code;
 	}
 	set sethtml(code){
+		console.log('sethtml');
 		document.body.innerHTML = code;
 	}
 	get initdom(){
