fa5f26868ffbfcf089183d3293a5be3c64d637c2
coco <squarehacker@kakao.com>
2022-07-06 18:54:15 +0900

    2022-07-06 18:54:15 업데이트!!!

--- src/cocoBottomSheetJS.js
+++ src/cocoBottomSheetJS.js
@@ -182,7 +182,8 @@ class cocoButtomSheetJS{
 			//up
 			console.log('up');
 			moveY = this.starttouchY - pageY;
-			let calc = this.translatePOS+moveY;
+			let calc = this.translatePOS - moveY;
+			console.log(calc);
 			let maxcalc = parseFloat('-'+wh);
 			if(maxcalc <= calc){
 				this.BSElement.style.transform = 'translate(0, ' + calc + 'px)';
