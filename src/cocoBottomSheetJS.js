d0330e6f85aaec1f1c69993df8f8f2a60c5baa0d
coco <squarehacker@kakao.com>
2022-07-06 13:55:15 +0900

    2022-07-06 13:55:15 업데이트!!!

--- src/cocoBottomSheetJS.js
+++ src/cocoBottomSheetJS.js
@@ -6,16 +6,19 @@ class cocoButtomSheetJS{
 		console.log('constructor');
 		this.code = code;
 	}
+	//htmlcode를 재정의
 	sethtml(code){
 		this.code = code;
 	}
+	//htmlcode내용을 확인
 	gethtml(){
 		return this.code;
 	}
+	//Bottomsheet를 출력
 	show(){
-		console.log(this.code);
 		document.querySelector('body').prepend(this.BSElement);
 	}
+	//Bottomsheet를 없애기
 	hide(){
 		
 	}
