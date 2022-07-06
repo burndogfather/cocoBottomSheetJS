983964ad7528ac60494d4c20c569371142673ef9
coco <squarehacker@kakao.com>
2022-07-06 18:56:15 +0900

    2022-07-06 18:56:15 업데이트!!!

--- src/cocoBottomSheetJS.js
+++ src/cocoBottomSheetJS.js
@@ -267,6 +267,7 @@ class cocoButtomSheetJS{
 	
 	//바텀시트를 출력
 	show(){
+		document.body.style.position = 'fixed';
 		let style = this.BSElement.getAttribute('style');
 		this.BSElement.setAttribute('style',style + "transform:translate(0px, -"+this.min+"px);");
 		
