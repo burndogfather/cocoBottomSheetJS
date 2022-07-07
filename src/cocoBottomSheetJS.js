d26bd2a9b1cd41f8299be98764f2f978cbf4ee1a
coco <squarehacker@kakao.com>
2022-07-07 23:19:15 +0900

    2022-07-07 23:19:15 업데이트!!!

--- src/cocoBottomSheetJS.js
+++ src/cocoBottomSheetJS.js
@@ -181,13 +181,16 @@ class cocoButtomSheetJS{
 			let maxcalc = parseFloat('-'+this.max);
 			let mincalc = parseFloat('-'+this.min);
 			let whcalc = parseFloat('-'+wh);
-			console.log(maxcalc+50 +' / '+ calc);
 			if(whcalc <= calc && maxcalc <= calc){
 				this.BSElement.style.transform = 'translate(0, ' + calc + 'px)';
 			}
 			if(mincalc+50 < calc){
 				this.moving = false;
 				this.hide();
+			}
+			if(maxcalc+50 > calc){
+				this.moving = false;
+				this.showmax();
 			}
 			
 		}
