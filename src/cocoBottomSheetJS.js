4c5dd7a15bc69e5eeda8cdc29a1d71508afbf999
coco <squarehacker@kakao.com>
2022-07-07 12:53:15 +0900

    2022-07-07 12:53:15 업데이트!!!

--- src/cocoBottomSheetJS.js
+++ src/cocoBottomSheetJS.js
@@ -34,11 +34,7 @@ class cocoButtomSheetJS{
 		}
 		this.BSElement = document.createElement('article');
 		this.BSElement.classList.add('cocoButtomSheet');
-		this.BShandleElement = document.createElement('button');
-		this.BShandleElement.classList.add('cocoButtomSheet_handle');
-		this.BSElement.appendChild(this.BShandleElement);
-		console.log(this.BShandleElement);
-		this.BSElement.innerHTML = this.code;
+		this.BSElement.innerHTML = '<button class="cocoButtomSheet_handle"></button>'+ this.code;
 		document.querySelector('body').prepend(this.BSElement);
 		
 		if(this.overlayer){
@@ -83,7 +79,7 @@ class cocoButtomSheetJS{
 		
 		//handle이벤트
 		if(this.ismobile){
-			this.BSElement.addEventListener('touchstart', (e)=>{
+			this.BSElement.querySelector('.cocoButtomSheet_handle').addEventListener('touchstart', (e)=>{
 				//클릭시작
 				this.touchstart(e);
 			});
