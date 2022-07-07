73c74566c49a6241a8104f057254462f8b79304b
coco <squarehacker@kakao.com>
2022-07-07 10:32:14 +0900

    2022-07-07 10:32:14 업데이트!!!

--- src/cocoBottomSheetJS.js
+++ src/cocoBottomSheetJS.js
@@ -282,10 +282,11 @@ class cocoButtomSheetJS{
 	
 	//바텀시트를 출력
 	show(){
+		this.BSoverElement.classList.add('cocoButtonSheet_fadein');
 		document.body.classList.add('cocoButtomSheetforbounse');
 		let style = this.BSElement.getAttribute('style');
 		this.BSElement.setAttribute('style',style + "transform:translate(0px, -"+this.min+"px);");
-		this.BSoverElement.classList.add('cocoButtonSheet_fadein');
+		
 		if(!this.is_show){
 			
 			this.is_show = true;
@@ -294,7 +295,7 @@ class cocoButtomSheetJS{
 	
 	//바텀시트를 없애기
 	hide(){
-		this.moving = false;
+		this.BSoverElement.classList.remove('cocoButtonSheet_fadein');
 		document.body.classList.remove('cocoButtomSheetforbounse');
 		let style = this.BSElement.getAttribute('style');
 		this.BSElement.setAttribute('style',style + "transform:translate(0px, -0px);");
