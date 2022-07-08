fa3967ebc87c372ed2c33a6efad76afa141c3167
coco <squarehacker@kakao.com>
2022-07-08 15:23:15 +0900

    2022-07-08 15:23:15 업데이트!!!

--- src/cocoBottomSheetJS.js
+++ src/cocoBottomSheetJS.js
@@ -92,6 +92,7 @@ class cocoButtomSheetJS{
 		//handle이벤트
 		if(this.ismobile){
 			this.BSElement.querySelector('.cocoButtomSheet_handle').addEventListener('touchstart', (e)=>{
+				alert('touchStart!');
 				e.stopImmediatePropagation();
 				e.stopPropagation();
 				//클릭시작
